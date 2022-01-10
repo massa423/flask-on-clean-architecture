741e5320c76bf8b35837f2b2c326bc238d92ca3b
Suzuki Masaya <ikachang-kawaii@nifty.com>
2022-01-10 14:11:15 +0900

    add test_get_user_usecase

---
+++ tests/applications/test_user_get_usecase.py
@@ -0,0 +1,26 @@
+import pytest
+from app.applications.inbound_dto.user_input import UserInput
+from app.applications.user_get_usecase import UserGetUsecaseImpl
+from app.domains.user import User
+from app.interfaces.gateways.user_repository import UserRepository
+
+
+class UserRepositoryMock(UserRepository):
+    def create_user(self, user: User) -> User:
+        return user
+
+    def find_user_by_name(self, user: User) -> User:
+        return user
+
+
+class TestUserCreate:
+    @pytest.mark.parametrize(
+        "name",
+        ["test1"],
+    )
+    def test_user_get(self, name):
+        uc = UserGetUsecaseImpl(UserRepositoryMock())
+
+        res = uc.handle(name)
+
+        assert res.name == name
