e271c3d03b161e6f9c2c7707682e925db34c80ad
Suzuki Masaya <ikachang-kawaii@nifty.com>
2022-01-10 15:10:16 +0900

    add test_user_domain

---
+++ tests/domains/test_user_domain.py
@@ -0,0 +1,37 @@
+import pytest
+from app.domains.user import User
+from datetime import datetime
+
+
+class TestUser:
+    @pytest.mark.freeze_time(datetime(2022, 1, 1, 23, 59, 59))
+    @pytest.mark.parametrize(
+        ("input", "expected"),
+        [
+            (
+                {
+                    "name": "test-user",
+                    "password": "password",
+                    "email": "test-user@example.com",
+                },
+                {
+                    "name": "test-user",
+                    "password": "password",
+                    "email": "test-user@example.com",
+                },
+            ),
+        ],
+    )
+    def test_user(self, input, expected):
+        now = datetime.now()
+
+        user = User(
+            name=input["name"], password=input["password"], email=input["email"], created_at=now, updated_at=now
+        )
+
+        assert user.name == expected["name"]
+        assert user.password != expected["password"]
+        assert user.get_raw_password() == expected["password"]
+        assert user.email == expected["email"]
+        assert user.created_at == now
+        assert user.updated_at == now
